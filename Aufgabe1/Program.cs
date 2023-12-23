44831523c49aaf43e6c90b8258814a3a3a602bcf
Podgaynyy <npodgaynyy@gmail.com>
2023-12-23 13:03:26 +0100

    first commit

---
+++ Aufgabe1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает позиции элемента
+//в двумерном массиве, и возвращает значение этого элемента или же указание,
+//что такого элемента нет.
+
+
+int[,] FillArray(int rows, int cols, int min, int max) //Программа, которая заполняет 2хмерный массив
+{
+    int[,] array =  new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i,j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+void PrintElement(int[,] array, int x, int y) //где х и y будут номерами строк и столбцов массива
+{
+    if (x < array.GetLength(0) || y < array.GetLength(1))
+    {
+        Console.WriteLine(array[x, y]); 
+    }
+    else { Console.WriteLine("индексы элемента находятся за пределами массива"); }
+}
+
+PrintElement((FillArray(5,5,0,100)),2,7);
---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задайте двумерный массив. Напишите программу,
+//которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] FillArray(int rows, int cols) //Заполняем массив
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+    return array;
+}
+
+int[,] ZamenaStrok(int[,] array) //Метод, который заменяет строки в массиве
+{
+    int row1 = 0; //Первая строка будет иметь индекс 0
+    int row2 = array.GetLength(1)-1; //а это индекс последней строки
+    for (int i = 0; i < array.GetLength(1); i++) //Итерируем столбцы
+    {
+        int temp = array[row1, i]; // временная переменная для значения столбца в строке
+        array[row1, i] = array[row2, i]; //меняем значения столбца в первой и последней строке
+        array[row2, i] = temp;
+    }
+    return array;
+}
+
+void PrintArray(int[,] array) //выводим массив на экран
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j= 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(4, 4); //создали массив
+PrintArray(array); //распечатали обычный массив
+PrintArray(ZamenaStrok(array));//распечатали массив с заменёнными строками
